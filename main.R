a7e17935590697a5bbeec273f56a4b702569df0d
Yuan He <yuanhe326@berkeley.edu>
2015-04-08 23:04:25 -0700

    Dummy down

--- main.R
+++ main.R
@@ -116,11 +116,67 @@ write_submission <- function(probs, model_name) {
 ## Source the algorithms:
 source("algorithms.R")
 
-load("C:/Users/yuan/src/jr_kaggle/data/data.rda")
-Xtrain = data$train
-Ytrain = data$ytrain
-Xtest = data$test
 
+###############DUMMY#############
+makeDummy = function(train, test){
+#Make dummies for categorical, and release:
+dum_release = model.matrix(~train[,1])
+train = cbind(dum_release[,-1], train[,-1])
+first_cat = grep("c_", colnames(train))[1]
+train1 = train[,c(1:(first_cat-1), (ncol(train)-2):ncol(train))]
+
+dum_release = model.matrix(~test[,1])
+test = cbind(dum_release[,-1], test[,-1])
+test1 = test[,c(1:(first_cat-1), (ncol(test)-2):ncol(test))]
+
+for (i in first_cat:(ncol(train)-3)){
+  if (length(levels(train[,i])) >= length(levels(test[,i]))){
+    dummy = model.matrix(~train[,i])
+    train1 = cbind(train1, dummy[,-1])
+  }
+  else{
+    extra = length(levels(test[,i]))-length(levels(train[,i]))
+    mat0 = matrix(rep(0,nrow(train)*extra), ncol=extra)
+    dummy = model.matrix(~train[,i])
+    train1 = cbind(train1, dummy[,-1], mat0)
+  }
+}
+
+for (i in first_cat:(ncol(train)-3)){
+  if (length(levels(test[,i])) >= length(levels(train[,i]))){
+    dummy = model.matrix(~test[,i])
+    test1 = cbind(test1, dummy[,-1])
+  }
+  else{
+    extra = length(levels(train[,i]))-length(levels(test[,i]))
+    mat0 = matrix(rep(0,nrow(test)*extra), ncol=extra)
+    dummy = model.matrix(~test[,i])
+    test1 = cbind(test1, dummy[,-1], mat0)
+  }
+}
+
+return(list(train1, test1))
+}
+
+load("C:/Users/yuan/src/jr_kaggle/data/data.rda")
+train = data$train
+test = data$test
+
+dummy = makeDummy(train,test)
+data_dummy_0.5 = list(dummy[[1]], dummy[[2]], data$ytrain, 0.5)
+names(data_dummy_0.5) = c("train", "test", "ytrain", "na_cutoff")
+save(data_dummy_0.5, file="data_dummy_0.5.Rdata")
+
+load("C:/Users/yuan/src/jr_kaggle/data/data_cutoff0.8.rda")
+train = data$train
+test = data$test
+
+dummy = makeDummy(train,test)
+data_dummy_0.8 = list(dummy[[1]], dummy[[2]], data$ytrain, 0.8)
+names(data_dummy_0.8) = c("train", "test", "ytrain", "na_cutoff")
+save(data_dummy_0.8, file="data_dummy_0.8.Rdata")
+
+##Model fitting
 library(glmnet)
 
 cv_mod_ridge <- cv.glmnet(Xtrain, as.matrix(Ytrain[,1]), alpha = 0)
